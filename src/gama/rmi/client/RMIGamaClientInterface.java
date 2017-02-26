ee2e0550e4d98095b402007e3639bcbcfb3bb59a
nnkhoa <n.nhukhoa2209@gmail.com>
2017-02-26 10:25:05 +0700

    add client interface

--- src/gama/rmi/client/RMIGamaClientInterface.java
+++ src/gama/rmi/client/RMIGamaClientInterface.java
@@ -10,4 +10,5 @@ public interface RMIGamaClientInterface extends Remote {
 	public String getName() throws RemoteException;
 	public void setName(String name) throws RemoteException;
 	public void recieveStringFromServer(String string) throws RemoteException;
+	
 }
